6c34251ae3b2afc3868d1ed6a50a8d0c52197572
Claudia Lemos <lemos.claudia007@gmail.com>
2024-06-10 23:54:27 +0530

    css and eventhandler update

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const container = document.querySelector("#container");
 
-const size = 16;
+const button = document.createElement("button");
+button.classList.add("btn");
+button.textContent = "No. of boxes?";
+
+let size = 0;
+
+button.addEventListener("click", () => {
+  prompt();
+});
+
+function prompt() {
+  size = prompt("Enter no. of boxes of x and y axis, max. 100");
+}
+
+container.appendChild(button);
 
 for (let i = 0; i < size; i++) {
   const canvas = document.createElement("div");
