8a1c0769e23911696cf5793c2e20c977759ec2ef
Claudia Lemos <lemos.claudia007@gmail.com>
2024-06-13 12:29:01 +0530

    div segregation and css added

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const container = document.querySelector("#container");
 
+const head = document.createElement("div");
+head.classList.add("head");
+container.appendChild(head);
+
+const canvas = document.createElement("div");
+canvas.classList.add("canvas");
+container.appendChild(canvas);
+
 const button = document.createElement("button");
 button.classList.add("btn");
 button.textContent = "No. of boxes?";
 
-let size = 0;
+head.appendChild(button);
+
+let size = 1;
 
 button.addEventListener("click", () => {
-  prompt();
+  getInput();
+  updatecanvas();
 });
 
-function prompt() {
-  size = prompt("Enter no. of boxes of x and y axis, max. 100");
+function getInput() {
+  size = parseInt(prompt("Enter no. of boxes for x and y axis, maximum 100"));
 }
+function updatecanvas() {
+  canvas.innerHTML = "";
 
-container.appendChild(button);
-
-for (let i = 0; i < size; i++) {
-  const canvas = document.createElement("div");
-  canvas.classList.add("canvas");
-  container.appendChild(canvas);
+  for (let i = 0; i < size; i++) {
+    const cells = document.createElement("div");
+    cells.classList.add("cells");
+    canvas.appendChild(cells);
+  }
 }
-
 console.log("created");
+console.log(size);
