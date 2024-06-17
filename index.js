df78ddb752e0d4259d46f167960ddc705186c35f
Mike Storey <mike@agile-learning.institute>
2024-06-17 13:22:03 -0400

    move cells-per-row to canvas

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function getColor() {
 function setSize() {
   const size = parseInt(prompt("Enter number of cells you want, maximum 100"));
   if (size > 1 && size <= 100) {
-    const container = document.querySelector("#container");
-    container.style.setProperty("--cells-per-row", size);
+    reset();
     updatecanvas(size);
   } else {
     alert("Input size is out of range - not between 1 and 100");
@@ -31,7 +30,7 @@ function setSize() {
 
 function updatecanvas(size) {
   const canvas = document.getElementById("canvas");
-  canvas.innerHTML = "";
+  canvas.style.setProperty("--cells-per-row", size);
   for (let i = 0; i < size**2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
